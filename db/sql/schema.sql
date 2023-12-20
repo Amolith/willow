46728ebe2676e392ccf573323973bfe16341d9a1
Amolith <amolith@secluded.site>
2023-12-20 14:32:33 -0500

    link SQL script for initial schema

--- db/sql/schema.sql
+++ db/sql/schema.sql
@@ -4,36 +4,36 @@
 
 CREATE TABLE users
 (
-    username   TEXT      NOT NULL PRIMARY KEY,
-    hash       TEXT      NOT NULL,
-    salt       TEXT      NOT NULL,
+    username TEXT NOT NULL PRIMARY KEY,
+    hash TEXT NOT NULL,
+    salt TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE sessions
 (
-    token      TEXT      NOT NULL PRIMARY KEY,
-    username   TEXT      NOT NULL,
-    expires    TIMESTAMP NOT NULL,
+    token TEXT NOT NULL PRIMARY KEY,
+    username TEXT NOT NULL,
+    expires TIMESTAMP NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE projects
 (
-    url        TEXT      NOT NULL PRIMARY KEY,
-    name       TEXT      NOT NULL,
-    forge      TEXT      NOT NULL,
-    version    TEXT      NOT NULL,
+    url TEXT NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    forge TEXT NOT NULL,
+    version TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE releases
 (
-    id          TEXT      NOT NULL PRIMARY KEY,
-    project_url TEXT      NOT NULL,
-    release_url TEXT      NOT NULL,
-    tag         TEXT      NOT NULL,
-    content     TEXT      NOT NULL,
-    date        TIMESTAMP NOT NULL,
-    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    id TEXT NOT NULL PRIMARY KEY,
+    project_url TEXT NOT NULL,
+    release_url TEXT NOT NULL,
+    tag TEXT NOT NULL,
+    content TEXT NOT NULL,
+    date TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
